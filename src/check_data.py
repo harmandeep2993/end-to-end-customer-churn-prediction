c7cb0b12c6de85a26ed6e75cd3c4326b1859f9c5
harmandeep2993 <harmandeep2993@gmail.com>
2025-11-07 10:49:45 +0100

    updated code comments

--- src/check_data.py
+++ src/check_data.py
@@ -4,16 +4,14 @@ import pandas as pd
 RAW_DATA_PATH = "./data/raw/Customer-Churn.csv"
 
 def load_dataset(path=RAW_DATA_PATH):
-    """
-    Load dataset from CSV.
-    """
+    
+    # Load dataset from CSV.
     df = pd.read_csv(path)
     return df
 
 def df_overview(df):
-    """
-    Print dataset overview: shape, dtypes, missing values.
-    """
+    
+    # Print dataset overview: shape, dtypes, missing values.
     print("\n=== Shape ===")
     print(df.shape)
 
@@ -23,8 +21,8 @@ def df_overview(df):
     print("\n=== Missing Values ===")
     print(df.isna().sum().sort_values(ascending=False).head(20))
 
-# xecution
-if __name__ == "__main__":
+# Execution
+if __name__ == '__main__':
     df = load_dataset()
     df_overview(df)
 
